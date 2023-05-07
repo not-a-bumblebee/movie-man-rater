d09d2ec6aeaf832e280faddeb64a7dabc302eb53
My Name <actualnicholas@gmail.com>
2023-05-06 22:39:09 -0700

    test

--- init.sh
+++ init.sh
@@ -7,8 +7,9 @@ npx playwright install chromium
 if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then
  
   echo "...Copying Playwright Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/playwright/$PLAYWRIGHT_BROWSERS_PATH
+  cp -R $XDG_CACHE_HOME/playwright/  $PLAYWRIGHT_BROWSERS_PATH
 else 
   echo "...Storing Playwright Cache in Build Cache" 
   cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
 fi
+
