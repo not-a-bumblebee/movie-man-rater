f3f75f529d0f1b03661d54d69e6722cc291ea525
My Name <actualnicholas@gmail.com>
2023-05-07 20:55:20 -0700

    21

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# export PLAYWRIGHT_BROWSERS_PATH="/opt/render/.cache/ms-playwright/"
+export PLAYWRIGHT_BROWSERS_PATH="/opt/render/.cache/ms-playwright/"
 
 npx playwright install chromium
+# npm run build
 
 echo "xdg_cache" $XDG_CACHE_HOME
 echo "Playwright path:" $PLAYWRIGHT_BROWSERS_PATH
@@ -11,7 +12,7 @@ echo "Playwright path:" $PLAYWRIGHT_BROWSERS_PATH
 # Store/pull Playwright cache with build cache
 if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then
   echo "...Copying Playwright Cache from Build Cache"
-  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
+  cp -R $XDG_CACHE_HOME/ms-playwright/ $PLAYWRIGHT_BROWSERS_PATH
 else
   echo "...Storing Playwright Cache in Build Cache"
   cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
